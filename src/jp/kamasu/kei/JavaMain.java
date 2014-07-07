888a817bda86d19ef62ca37573bcc397c725e14a
Miyatin <kei@kamasu.jp>
2014-07-08 02:43:01 +0900

    Remove comment out

--- src/jp/kamasu/kei/JavaMain.java
+++ src/jp/kamasu/kei/JavaMain.java
@@ -6,12 +6,9 @@ import java.util.List;
 
 public class JavaMain {
 
-	/* 繝｡繧､繝ｳ蜃ｦ逅�*/
 	public static void main(String[] args) {
 		String[] str = {"test"};
-		// TODO 閾ｪ蜍慕函謌舌＆繧後◆繝｡繧ｽ繝�ラ繝ｻ繧ｹ繧ｿ繝�		]
 		System.out.println("Hello World!");
-
 
 		Matrix.makeMatrix(10,10);
 
